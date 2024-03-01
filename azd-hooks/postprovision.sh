285dfdaa1db1ad568b7701f3128300c445b7ce17
Toru Makabe <993850+torumakabe@users.noreply.github.com>
2024-03-01 16:00:34 +0900

    Update postprovision.sh

--- azd-hooks/postprovision.sh
+++ azd-hooks/postprovision.sh
@@ -6,7 +6,7 @@ if [ "$DEPLOY_AZURE_CONTAINER_REGISTRY" == "true" ] && [ "$BUILD_CONTAINERS" == 
   echo "Build container images"
   for service in "${services[@]}"; do
     echo "Building aks-store-demo/${service}:latest"
-    az acr build --registry ${AZURE_REGISTRY_NAME} --image aks-store-demo/${service}:latest ./src/${service}/
+    az acr build -g ${AZURE_RESOURCEGROUP_NAME} --registry ${AZURE_REGISTRY_NAME} --image aks-store-demo/${service}:latest ./src/${service}/
   done
 elif [ "$DEPLOY_AZURE_CONTAINER_REGISTRY" == "true" ] && ([ -z "$BUILD_CONTAINERS" ] || [ "$BUILD_CONTAINERS" == "false" ]); then
   echo "Import container images"
@@ -14,6 +14,6 @@ elif [ "$DEPLOY_AZURE_CONTAINER_REGISTRY" == "true" ] && ([ -z "$BUILD_CONTAINER
     echo "Importing aks-store-demo/${service}:latest"
     az acr import --name ${AZURE_REGISTRY_NAME} --source ghcr.io/azure-samples/aks-store-demo/${service}:latest --image aks-store-demo/${service}:latest
   done
-else 
+else
   echo "No BUILD_CONTAINERS variable set, skipping container build/import"
 fi
